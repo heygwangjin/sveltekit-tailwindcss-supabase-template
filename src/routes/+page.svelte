59e54f79ab86c40f08105750e5d813c5a7b79110
heygwangjin <heygwangjin@gmail.com>
2023-06-28 20:55:48 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl text-orange-500 font-bold">
+	Welcome to Sveltekit + TailwindCSS + Supabase Template
+</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
